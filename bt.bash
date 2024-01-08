58a590c532787daa2dd5540b771d3312722f1db2
Tyler Philbrick <git@choam.me>
2024-01-08 11:41:00 +0000

    Permit dump and bt to pull from other files

--- bt.bash
+++ bt.bash
@@ -2,9 +2,26 @@
 
 set -euo pipefail
 
-cat last_output | \
+file_name="build/cardinal3"
+
+while getopts "f:u" opt; do
+  case $opt in
+    f)
+      file_name=$OPTARG
+      ;;
+    u)
+      file_name="build/userland"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+< last_output \
     grep '(.*) <.*>' | \
     sed 's/.*(\(.*\)).*/\1/g' | \
-    xargs llvm-addr2line -fips -e build/cardinal3 | \
+    xargs llvm-addr2line -fips -e "$file_name" | \
     rustfilt
 
--- dump.bash
+++ dump.bash
@@ -2,4 +2,20 @@
 
 set -euo pipefail
 
-llvm-objdump -d build/cardinal3 | rustfilt | less
+file_name="build/cardinal3"
+
+while getopts "f:u" opt; do
+  case $opt in
+    f)
+      file_name="$OPTARG"
+      ;;
+    u)
+      file_name="build/userland"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+llvm-objdump -d $file_name | rustfilt | less
